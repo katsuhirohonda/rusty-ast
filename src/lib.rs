cac7927e0fa5d370ac87a03311f3204b6a0ccdb7
Katsuhiro Honda <freewave3@gmail.com>
2025-02-26 06:37:04 +0900

    Add rusty-ast binary and library for AST visualization

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+//! library for parsing Rust code and visualizing AST
+//!
+//! This crate provides tools for parsing Rust source code and displaying its abstract syntax tree (AST).
+
+mod parser;
+mod visitor;
